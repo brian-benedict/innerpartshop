5391d86032e599c84e5b990fcfcebbbc999340fb
Brian Benedict Njau <85028420+brian-benedict@users.noreply.github.com>
2023-12-10 15:10:57 +0300

    vercel deployment

--- innerpartshop/settings.py
+++ innerpartshop/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-#s-ducxjo*8f-ms7-lafwh48#hj(thkdehsdfxn!&s0(6-)ln(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 # Application definition
 
